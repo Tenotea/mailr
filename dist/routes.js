4070145fa7825af3ec4428ea04e94ca677ec1614
Tenotea <drtenotea@gmail.com>
2020-08-04 16:54:28 +0100

    Removed distribution folder from git push. Api will be compiled when initialzed

--- dist/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-var SignIn_1 = require("./routes/SignIn");
-exports.routes = function (app) {
-    app.post('/sign-in', SignIn_1.SignIn);
-};
--- dist/routes/home.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SignIn = void 0;
-var express_1 = require("express");
-exports.SignIn = express_1.Router();
-exports.SignIn.get('/sign-in', function (req, res) {
-    res.send('User is being signed in here');
-});
